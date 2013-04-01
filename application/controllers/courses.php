cd0b1cb6d9d00ed76a8eaef809bb927e02d2284d
Leo Jeremiah Vicente <jemnuineuron@live.com>
2013-04-02 03:51:57 +0800

    course controller added

--- application/controllers/courses.php
+++ application/controllers/courses.php
@@ -24,16 +24,16 @@ class Courses extends CI_Controller {
 
 			);
 
-			$this->load->model('schedule_model');
+			$this->load->model('course_model');
 
-            if($query = $this->schedule_model->list_course()) {
+            if($query = $this->course_model->list_department()) {
                 $data['records'] = $query;
             }
 
 			$this->load->view('includes/nocache');
 	        $this->load->view('includes/header2');
-	        $this->load->view('courses_view', $data);
-	        $this->load->view('includes/footer2');
+	        $this->load->view('departments_view', $data);
+	        $this->load->view('includes/department_footer');
 
 			
 		}
@@ -44,5 +44,147 @@ class Courses extends CI_Controller {
 			redirect(base_url(), 'refresh');
 		}
  	}
+
+ 	public function add_course() {
+
+        $this->form_validation->set_rules('addCode','Department Code','trim|required');
+        $this->form_validation->set_rules('addDesc','Department Description','trim|required');
+        
+
+        if($this->form_validation->run() == TRUE) {
+            $data = array (
+                'department_code' => $this->input->post('addCode'),
+                'department_desc' => $this->input->post('addDesc'),
+                'userid' => $this->session->userdata('userid')   
+            );
+
+
+            $this->load->model('course_model');
+            $this->course_model->add_course($data);
+
+            $data = array (
+                'current_user' => $this->session->userdata('displayname'),
+                'current_username' => $this->session->userdata('username'),
+            	'add_course_error_msg' => NULL,
+            	'add_course_error_action' => NULL //wala lang xD
+            );
+
+            redirect(base_url() . 'index.php/departments', 'refresh');   
+            
+        }
+        else
+        {
+
+            $add_course_error_msg = '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>' . validation_errors() . '</div>';
+            $add_course_error_action = "$('#modalAddDepartment').modal('show');";
+
+            $data = array (
+                'current_user' => $this->session->userdata('displayname'),
+                'current_username' => $this->session->userdata('username'),
+                'add_course_error_msg' => $add_course_error_msg,
+                'add_course_error_action' => $add_course_error_action
+            );
+
+            $this->load->model('course_model');
+
+            if($query = $this->course_model->list_department()) {
+                $data['records'] = $query;
+            }
+
+            if(!$data['records']) {
+                $add_course_error_msg = '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>The record is existing!</div>';
+                $add_course_error_action = "$('#modalAddDepartment').modal('show');";
+                $data = array (
+                    'current_user' => $this->session->userdata('displayname'),
+                    'current_username' => $this->session->userdata('username'),
+                    'add_course_error_msg' => $add_course_error_msg,
+                    'add_course_error_action' => $add_course_error_action
+                );
+            }
+             
+	        
+
+            $this->session->set_userdata($data);
+
+			$this->load->view('includes/nocache');
+			$this->load->view('includes/header2');
+    		$this->load->view('departments_view', $data);
+    		$this->load->view('includes/department_footer', $data);
+
+            
+        }
+    }
+
+    public function list_edit_department (
+            $add_course_error_msg = NULL, 
+            $add_course_error_action = "$('#modalEditSemester').modal('show');", 
+            $reg_error_msg = NULL
+
+        ) {
+
+        $data = array (
+                'department_code' => $this->input->post('addCode'),
+                'department_desc' => $this->input->post('addDescription'),
+                'userid' => $this->session->userdata('userid')
+            );
+
+
+        $this->load->model('course_model');
+        //kinuha lang ung department id galing sa view
+        $department_id = $this->input->post('dataid');
+
+        //check kung ajax request
+        if($this->input->post('ajax')) {
+
+            //ni-recycle ko lng ung sa addsem na modal trigger
+            $add_course_error_action = "$('#modalEditDepartment').modal('show');"; 
+
+            $query = $this->course_model->get_dept_code($department_id);
+            $query2 = $this->course_model->get_dept_desc($department_id);
+
+            $data = array (
+                'current_user' => $this->session->userdata('displayname'),
+                'current_username' => $this->session->userdata('username'),
+                'add_course_error_msg' => $add_course_error_msg,
+                'add_course_error_action' => $add_course_error_action,
+                'dataid' => $department_id,
+                'dcode' => $query,
+                'ddesc' => $query2
+                
+            );
+
+            $this->session->set_userdata($data); //the trick!! mawawala na ung ajax request next time eh
+            
+            echo implode('', $data['dcode']);
+            echo '*';
+            echo implode('', $data['ddesc']);
+        } else {
+
+            $code = $this->input->post('editCode');
+            $desc = $this->input->post('editDesc');
+
+            //kinuha ung session ng dept id
+            $department_id = $this->session->userdata('dataid');
+            $this->course_model->update_department($department_id, $code, $desc);
+            redirect(base_url().'index.php/departments');
+        }
+
+    }
+
+    public function delete_department ($id = NULL) {
+
+        $this->load->model('course_model');
+        $this->course_model->delete_department($id);
+        redirect(base_url().'index.php/departments');
+        return;
+    }
+
+    public function delete_all_department () {
+
+        $this->load->model('course_model');
+        $this->course_model->delete_all_department();
+        redirect(base_url().'index.php/departments');
+        return;
+    }
 
 }
---
+++ application/models/course_model.php
@@ -0,0 +1,107 @@
+<?php
+
+class Course_model extends CI_Model {
+
+    function __construct() {
+        parent:: __construct();
+    }
+
+     /********** Begin Department Functions **********/
+
+    public function add_course($data) {
+        
+        //check if may duplicate row
+        $query = $this->db->query (
+            'SELECT * FROM department WHERE userid=' . $this->session->userdata('userid') .
+            ' AND department_code="' . $data['department_code'] . '" AND department_desc="' . $data['department_desc'] . '"'
+            );
+        $records = $query->result();
+
+        //if may record
+        if($records) {
+            return false;
+        }
+
+        $this->db->insert('department', $data);
+        return true;
+    }
+
+    public function list_department() {
+
+        $query = $this->db->query('SELECT * FROM department WHERE userid='.$this->session->userdata('userid') . ' ORDER BY department_code ASC');
+        return $query->result();
+    }
+
+    public function get_dept_code($param) {
+
+        $this->db->where('department_id', $param);
+        $this->db->select('department_code');
+        $query = $this->db->get('department');
+        return $query->row_array();
+    }
+
+    public function get_dept_desc($param) {
+
+        $this->db->where('department_id', $param);
+        $this->db->select('department_desc');
+        $query = $this->db->get('department');
+        return $query->row_array();
+    }
+
+
+    public function update_department($id, $code, $desc) {
+
+        //check if may duplicate row
+        $query = $this->db->query (
+            'SELECT department_code, department_desc FROM department WHERE userid=' . $this->session->userdata('userid') .
+            ' AND department_code="' . $code . '" OR department_desc="' . $desc . '"'
+            );
+        $records = $query->result();
+
+        //if may record
+        if($records) {
+            return false;
+        }
+
+        if($id != '') {
+
+            $data = array (
+                'department_code' => $code,
+                'department_desc' => $desc
+            );
+
+            $this->db->where('department_id',$id);
+            $this->db->update('department', $data);
+
+        } else {
+
+            echo 'Oops! Something is wrong in updating Department :(';
+        }
+    }
+
+    public function delete_department($id) {
+        
+        if($id != '') {
+
+            $this->db->where('department_id',$id);
+            $this->db->delete('department');
+
+            $query = $this->db->query('SELECT department_desc FROM department WHERE userid='.$this->session->userdata('userid') . ' AND department_id=' . $id);
+            $desc = $query->result();
+            //cascade deletion
+            $this->db->where('department_desc', $desc['department_desc']);
+            $this->db->delete('course');
+
+        } else {
+
+            echo 'Oops! Something is wrong in deleting semester :(';
+        }
+    }
+
+    public function delete_all_department() {
+        $this->db->where('userid', $this->session->userdata('userid'));
+        $this->db->delete('department'); 
+    }
+
+    /********** End Department Functions **********/
+}
